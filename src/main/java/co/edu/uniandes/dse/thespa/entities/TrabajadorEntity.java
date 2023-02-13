2a001fcfc00c05f37a88b7e832f71ddf5562d8a4
Daniel27110 <d.vargasu@uniandes.edu.co>
2023-02-12 20:43:17 -0500

    Beneficio Repository Done

--- src/main/java/co/edu/uniandes/dse/thespa/entities/TrabajadorEntity.java
+++ src/main/java/co/edu/uniandes/dse/thespa/entities/TrabajadorEntity.java
@@ -2,6 +2,7 @@ package co.edu.uniandes.dse.thespa.entities;
 
 import java.io.File;
 import javax.persistence.ManyToMany;
+
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -24,4 +25,7 @@ public class TrabajadorEntity extends BaseEntity {
 
     @ManyToMany
     private List<ServicioEntity> sedes;
+
+    @ManyToMany
+    private List<ServicioEntity> servicios;
 }
