01ec4c384f94e022473e30bc9ccb320a6fef8cc4
Manuela Pacheco Malagón <m.pachecom2@uniandes.edu.co>
2023-02-20 21:59:39 -0500

    Servicio - Trabajador Test

--- src/main/java/co/edu/uniandes/dse/thespa/services/ServicioService.java
+++ src/main/java/co/edu/uniandes/dse/thespa/services/ServicioService.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import co.edu.uniandes.dse.thespa.entities.ServicioEntity;
+import co.edu.uniandes.dse.thespa.entities.TrabajadorEntity;
 import co.edu.uniandes.dse.thespa.exceptions.EntityNotFoundException;
 import co.edu.uniandes.dse.thespa.exceptions.IllegalOperationException;
-import co.edu.uniandes.dse.thespa.repositories.PackDeServiciosRepository;
+import co.edu.uniandes.dse.thespa.repositories.TrabajadorRepository;
 import co.edu.uniandes.dse.thespa.repositories.SedeRepository;
 import co.edu.uniandes.dse.thespa.repositories.ServicioRepository;
-import co.edu.uniandes.dse.thespa.repositories.TrabajadorRepository;
+import co.edu.uniandes.dse.thespa.repositories.PackDeServiciosRepository;
+
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
@@ -126,5 +128,58 @@ public class ServicioService {
         }
         servicioRepository.deleteById(servicioId);
         log.info("Termina proceso de borrar el servicio con id = {0}", servicioId);
+    }
+
+    @Transactional
+    public TrabajadorEntity addTrabajador(Long servicioID, long trabajadorID) throws EntityNotFoundException, IllegalOperationException {
+        log.info("Inicia proceso de agregar un trabajador al servicio con id = {0}", servicioID);
+        Optional<ServicioEntity> servicioEntity = servicioRepository.findById(servicioID);
+        if (servicioEntity.isEmpty()) {
+            throw new EntityNotFoundException("El servicio con el id = " + servicioID + " no existe");
+        }
+        Optional<TrabajadorEntity> trabajadorEntity = trabajadorRepository.findById(trabajadorID);
+        if (trabajadorEntity.isEmpty()) {
+            throw new EntityNotFoundException("El trabajador con el id = " + trabajadorID + " no existe");
+        }
+
+        if (servicioEntity.get().getTrabajadores().contains(trabajadorEntity.get())) {
+            throw new IllegalOperationException("El trabajador con el id = " + trabajadorID + " ya está asociado al servicio con el id = " + servicioID);
+        }
+        servicioEntity.get().getTrabajadores().add(trabajadorEntity.get());
+        log.info("Termina proceso de agregar un trabajador al servicio con id = {0}", servicioID);
+
+        return trabajadorEntity.get();
+    }
+
+    @Transactional
+    public List<TrabajadorEntity> getTrabajadores(Long servicioID) throws EntityNotFoundException {
+        log.info("Inicia proceso de consultar los trabajadores del servicio con id = {0}", servicioID);
+        Optional<ServicioEntity> servicioEntity = servicioRepository.findById(servicioID);
+        if (servicioEntity.isEmpty()) {
+            throw new EntityNotFoundException("El servicio con el id = " + servicioID + " no existe");
+        }
+        log.info("Termina proceso de consultar los trabajadores del servicio con id = {0}", servicioID);
+        return servicioEntity.get().getTrabajadores();
+    }
+
+    @Transactional
+    public TrabajadorEntity removeTrabajador(Long servicioID, long trabajadorID) throws EntityNotFoundException, IllegalOperationException {
+        log.info("Inicia proceso de remover un trabajador del servicio con id = {0}", servicioID);
+        Optional<ServicioEntity> servicioEntity = servicioRepository.findById(servicioID);
+        if (servicioEntity.isEmpty()) {
+            throw new EntityNotFoundException("El servicio con el id = " + servicioID + " no existe");
+        }
+        Optional<TrabajadorEntity> trabajadorEntity = trabajadorRepository.findById(trabajadorID);
+        if (trabajadorEntity.isEmpty()) {
+            throw new EntityNotFoundException("El trabajador con el id = " + trabajadorID + " no existe");
+        }
+
+        if (!servicioEntity.get().getTrabajadores().contains(trabajadorEntity.get())) {
+            throw new IllegalOperationException("El trabajador con el id = " + trabajadorID + " no está asociado al servicio con el id = " + servicioID);
+        }
+        servicioEntity.get().getTrabajadores().remove(trabajadorEntity.get());
+        log.info("Termina proceso de remover un trabajador del servicio con id = {0}", servicioID);
+
+        return trabajadorEntity.get();
     }
 }
---
+++ src/test/java/co/edu/uniandes/dse/thespa/services/ServicioAndTrabajadorServiceTest.java
@@ -0,0 +1,128 @@
+package co.edu.uniandes.dse.thespa.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
+
+import co.edu.uniandes.dse.thespa.entities.TrabajadorEntity;
+import co.edu.uniandes.dse.thespa.entities.ServicioEntity;
+import co.edu.uniandes.dse.thespa.exceptions.EntityNotFoundException;
+import co.edu.uniandes.dse.thespa.exceptions.IllegalOperationException;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+@DataJpaTest
+@Transactional
+@Import({ ServicioService.class, TrabajadorService.class })
+
+public class ServicioAndTrabajadorServiceTest {
+
+    @Autowired
+    private ServicioService servicioService;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    private PodamFactory factory = new PodamFactoryImpl();
+
+    private List<ServicioEntity> servicios = new ArrayList<>();
+
+    private List<TrabajadorEntity> trabajadores = new ArrayList<>();
+
+
+    @BeforeEach
+    void setUp() {
+        clearData();
+        insertData();
+    }
+
+    private void clearData() {
+        entityManager.clear();
+        entityManager.getEntityManager().createQuery("delete from ServicioEntity").executeUpdate();
+        entityManager.getEntityManager().createQuery("delete from TrabajadorEntity").executeUpdate();
+    }
+
+    private void insertData() {
+        for (int i = 0; i < 3; i++) {
+            ServicioEntity servicio = factory.manufacturePojo(ServicioEntity.class);
+            entityManager.persist(servicio);
+            servicios.add(servicio);
+            servicio.setTrabajadores(new ArrayList<>());
+            for (int j = 0; j < 3; j++) {
+                TrabajadorEntity trabajador = factory.manufacturePojo(TrabajadorEntity.class);
+                entityManager.persist(trabajador);
+                trabajadores.add(trabajador);
+                servicio.getTrabajadores().add(trabajador);
+            }
+        }
+        
+    }
+
+    @Test
+    void addTrabajadorTest() throws EntityNotFoundException, IllegalOperationException {
+        ServicioEntity servicio = servicios.get(0);
+        TrabajadorEntity trabajador = factory.manufacturePojo(TrabajadorEntity.class);
+        entityManager.persist(trabajador);
+        trabajadores.add(trabajador);
+
+        TrabajadorEntity result = servicioService.addTrabajador(servicio.getId(), trabajador.getId());
+
+        assertNotNull(result);
+        assertEquals(trabajador, result);
+    }
+
+    @Test
+    void addTrabajadorAlreadyInServicioTest() throws EntityNotFoundException, IllegalOperationException {
+        ServicioEntity servicio = servicios.get(0);
+        TrabajadorEntity trabajador = trabajadores.get(0);
+
+        assertThrows(IllegalOperationException.class, () -> {
+            servicioService.addTrabajador(servicio.getId(), trabajador.getId());
+        });
+    }
+
+    @Test
+    void getTrabajadoresTest() throws EntityNotFoundException {
+        ServicioEntity servicio = servicios.get(0);
+
+        List<TrabajadorEntity> result = servicioService.getTrabajadores(servicio.getId());
+
+        assertNotNull(result);
+        assertEquals(servicio.getTrabajadores(), result);
+    }
+
+    @Test
+    void removeTrabajadorTest() throws EntityNotFoundException, IllegalOperationException {
+        ServicioEntity servicio = servicios.get(0);
+        TrabajadorEntity trabajador = trabajadores.get(0);
+
+        servicioService.removeTrabajador(servicio.getId(), trabajador.getId());
+
+        List<TrabajadorEntity> result = servicioService.getTrabajadores(servicio.getId());
+        assertNotNull(result);
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    void removeTrabajadorNotInServicioTest() throws EntityNotFoundException, IllegalOperationException {
+        ServicioEntity servicio = servicios.get(0);
+        TrabajadorEntity trabajador = factory.manufacturePojo(TrabajadorEntity.class);
+        entityManager.persist(trabajador);
+        trabajadores.add(trabajador);
+        
+        assertThrows(IllegalOperationException.class, () -> {
+            servicioService.removeTrabajador(servicio.getId(), trabajador.getId());
+        });
+    }
+}
