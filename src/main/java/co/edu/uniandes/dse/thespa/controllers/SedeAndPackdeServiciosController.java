c54781e815cc450487439c0b5b0c5901df6a655e
Daniel27110 <d.vargasu@uniandes.edu.co>
2023-03-22 15:23:33 -0500

    SedesPacksTestsDone

--- src/main/java/co/edu/uniandes/dse/thespa/controllers/SedeAndPackdeServiciosController.java
+++ src/main/java/co/edu/uniandes/dse/thespa/controllers/SedeAndPackdeServiciosController.java
@@ -3,11 +3,14 @@ package co.edu.uniandes.dse.thespa.controllers;
 import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,14 +26,16 @@ import co.edu.uniandes.dse.thespa.entities.PackDeServiciosEntity;
 public class SedeAndPackdeServiciosController {
 
     // inyectar el servicio de sedes y packs de servicios
+    @Autowired
     private SedeAndPackServicesService saP;
 
     // inyecta el model mapper
+    @Autowired
     private ModelMapper modelMapper;
 
     // metodo para encontrar todos los packs de servicios dentro de una sede dado su
     // id
-    @GetMapping(value = "/{id}/packs")
+    @GetMapping(value = "{id}/packs")
     @ResponseStatus(code = HttpStatus.OK)
     public List<PackDeServiciosDetailDTO> findAll(@PathVariable("id") Long id) {
         List<PackDeServiciosEntity> servicios = saP.obtenerAllPacks(id);
@@ -39,17 +44,17 @@ public class SedeAndPackdeServiciosController {
     }
 
     // Busca un pack de servicios dentro de los asociados a una sede
-	@GetMapping(value = "/{id}/packs/{idPack}")
-	@ResponseStatus(code = HttpStatus.OK)
-	public PackDeServiciosDetailDTO getServicio(@PathVariable("id") Long sedeId,
-			@PathVariable("idPack") Long packId)
-			throws EntityNotFoundException, IllegalOperationException {
+    @GetMapping(value = "{id}/packs/{idPack}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public PackDeServiciosDetailDTO getServicio(@PathVariable("id") Long sedeId,
+            @PathVariable("idPack") Long packId)
+            throws EntityNotFoundException, IllegalOperationException {
         PackDeServiciosEntity pack = saP.getPack(sedeId, packId);
-		return modelMapper.map(pack, PackDeServiciosDetailDTO.class);
-	}
+        return modelMapper.map(pack, PackDeServiciosDetailDTO.class);
+    }
 
     // metodo para agregar un pack de servicios a una sede dado su id
-    @PostMapping(value = "/{id}/packs/{idPack}")
+    @PostMapping(value = "{id}/packs/{idPack}")
     @ResponseStatus(code = HttpStatus.CREATED)
     public PackDeServiciosDTO create(@PathVariable("id") Long id, @PathVariable("idPack") Long idPack)
             throws IllegalOperationException, EntityNotFoundException {
@@ -59,13 +64,28 @@ public class SedeAndPackdeServiciosController {
     }
 
     // metodo para eliminar un pack servicio de una sede dado su id
-    @DeleteMapping(value = "/{id}/packs/{idPack}")
+    @DeleteMapping(value = "{id}/packs/{idPack}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public PackDeServiciosDTO delete(@PathVariable("id") Long id, @PathVariable("idPack") Long idPack)
             throws IllegalOperationException, EntityNotFoundException {
 
         PackDeServiciosEntity servicioEliminado = saP.deleteSedePackDeServicios(id, idPack);
         return modelMapper.map(servicioEliminado, PackDeServiciosDTO.class);
+    }
+
+    // metodo para actualizar la lista de packs de servicios de una sede
+    @PutMapping(value = "{id}/packs")
+    @ResponseStatus(code = HttpStatus.OK)
+    public List<PackDeServiciosDetailDTO> update(@PathVariable("id") Long id,
+            @RequestBody List<PackDeServiciosDetailDTO> packs)
+            throws IllegalOperationException, EntityNotFoundException {
+
+        List<PackDeServiciosEntity> packsEntity = modelMapper.map(packs,
+                new TypeToken<List<PackDeServiciosEntity>>() {
+                }.getType());
+        List<PackDeServiciosEntity> packsActualizados = saP.updateSedePackDeServicios(id, packsEntity);
+        return modelMapper.map(packsActualizados, new TypeToken<List<PackDeServiciosDetailDTO>>() {
+        }.getType());
     }
 
 }
