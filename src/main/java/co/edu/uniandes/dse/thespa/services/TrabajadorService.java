b6b88617706a42ba9baf43ec566786fe99328cf3
n-rincon4 <n.rincon4@uniandes.edu.co>
2023-03-22 21:09:42 -0500

    Pruebas postman y otros tweaks

--- src/main/java/co/edu/uniandes/dse/thespa/services/TrabajadorService.java
+++ src/main/java/co/edu/uniandes/dse/thespa/services/TrabajadorService.java
@@ -34,7 +34,7 @@ public class TrabajadorService {
         if ((trabajadorEntity.getNombre() == null) || (trabajadorEntity.getNombre().equals("")))
             throw new IllegalOperationException("El nombre del trabajador no puede estar vacío");
 
-        if (trabajadorEntity.getSedes() == null)
+        if ((trabajadorEntity.getSedes() == null) || (trabajadorEntity.getSedes().size() == 0))
             throw new IllegalOperationException("El trabajador debe estar en al menos una sede");
 
         if (trabajadorEntity.getServicios() == null)
