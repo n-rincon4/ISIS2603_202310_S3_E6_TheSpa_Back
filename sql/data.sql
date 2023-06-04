ca0236696ebb7569cc9f7c8b0a5903aaf8e0a9e6
Daniel27110 <d.vargasu@uniandes.edu.co>
2023-06-03 19:34:26 -0500

    Updated sql

--- sql/data.sql
+++ sql/data.sql
@@ -16,17 +16,17 @@ DELETE FROM UBICACION_ENTITY;
 
 -- Ubicaciones
 INSERT INTO UBICACION_ENTITY (id, latitud, longitud, ciudad, direccion)
-VALUES (1, 4.603014, 74.065227, 'Bogotá', 'Cl. 20 #19a-71 a 19a-1, Bogotá');
+VALUES (1, 4.603014, 74.065227, 'Bogotá', 'Cl. 20 #19a-71 a 19a-1');
 INSERT INTO UBICACION_ENTITY (id, latitud, longitud, ciudad, direccion)
-VALUES (2, 4.712345, 74.987654, 'Medellín', 'Av. Principal #123, Medellín');
+VALUES (2, 4.712345, 74.987654, 'Medellín', 'Av. Principal #123');
 INSERT INTO UBICACION_ENTITY (id, latitud, longitud, ciudad, direccion)
 VALUES (3, 4.520202, 74.033393, 'Cali', 'Cra. 50 #10-20, Cali');
 INSERT INTO UBICACION_ENTITY (id, latitud, longitud, ciudad, direccion)
-VALUES (4, 4.813459, 74.115678, 'Barranquilla', 'Cl. 80 #5-10, Barranquilla');
+VALUES (4, 4.813459, 74.115678, 'Barranquilla', 'Cl. 80 #5-10');
 INSERT INTO UBICACION_ENTITY (id, latitud, longitud, ciudad, direccion)
-VALUES (5, 4.634567, 74.789012, 'Cartagena', 'Cra. 2 #15-30, Cartagena');
+VALUES (5, 4.634567, 74.789012, 'Cartagena', 'Cra. 2 #15-30');
 INSERT INTO UBICACION_ENTITY (id, latitud, longitud, ciudad, direccion)
-VALUES (6, 4.432156, 74.222333, 'Santa Marta', 'Av. Playa #8-12, Santa Marta');
+VALUES (6, 4.432156, 74.222333, 'Santa Marta', 'Av. Playa #8-12');
 
 -- Sedes
 INSERT INTO SEDE_ENTITY (id, nombre, imagen, ubicacion_id)
@@ -44,29 +44,29 @@ VALUES (6, 'Spa del Bosque', 'https://www.visit.alsace/wp-content/uploads/lei/pi
 
 -- Servicios
 INSERT INTO SERVICIO_ENTITY (id, duracion, restricciones, disponible, hora_inicio, nombre, descripcion, precio, imagen, sede_id)
-VALUES (1, '3', 'Ser mayor de 16 años', true, 8, 'masaje', 'masaje de relajacion', '30000', 'https://www.genesiscareer.edu/wp-content/uploads/2019/06/swedishmassage.jpg', 1);
+VALUES (1, '3', 'Ser mayor de 16 años', true, 8, 'Masaje', 'Masaje de relajacion', '30000', 'https://i.pinimg.com/originals/5e/5c/bd/5e5cbd4d3517ba13db403b2145f91b07.jpg', 1);
 INSERT INTO SERVICIO_ENTITY (id, duracion, restricciones, disponible, hora_inicio, nombre, descripcion, precio, imagen, sede_id)
-VALUES (2, '4', 'Ser mayor de 18 años', true, 9, 'facial', 'tratamiento facial', '50000', 'https://www.genesiscareer.edu/wp-content/uploads/2019/06/swedishmassage.jpg', 1);
+VALUES (2, '4', 'Ser mayor de 18 años', true, 9, 'Facial', 'Tratamiento facial', '50000', 'https://i.pinimg.com/originals/5e/5c/bd/5e5cbd4d3517ba13db403b2145f91b07.jpg', 1);
 INSERT INTO SERVICIO_ENTITY (id, duracion, restricciones, disponible, hora_inicio, nombre, descripcion, precio, imagen, sede_id)
-VALUES (3, '2', 'Ser mayor de 18 años', true, 10, 'manicure', 'manicure y pedicure', '25000', 'https://www.genesiscareer.edu/wp-content/uploads/2019/06/swedishmassage.jpg', 2);
+VALUES (3, '2', 'Ser mayor de 18 años', true, 10, 'Manicure', 'Manicure y pedicure', '25000', 'https://i.pinimg.com/originals/5e/5c/bd/5e5cbd4d3517ba13db403b2145f91b07.jpg', 2);
 INSERT INTO SERVICIO_ENTITY (id, duracion, restricciones, disponible, hora_inicio, nombre, descripcion, precio, imagen, sede_id)
-VALUES (4, '1', 'No aplican restricciones', true, 11, 'depilacion', 'depilacion de cejas', '15000', 'https://www.genesiscareer.edu/wp-content/uploads/2019/06/swedishmassage.jpg', 2);
+VALUES (4, '1', 'No aplican restricciones', true, 11, 'Depilacion', 'Depilacion de cejas', '15000', 'https://i.pinimg.com/originals/5e/5c/bd/5e5cbd4d3517ba13db403b2145f91b07.jpg', 2);
 INSERT INTO SERVICIO_ENTITY (id, duracion, restricciones, disponible, hora_inicio, nombre, descripcion, precio, imagen, sede_id)
-VALUES (5, '2', 'No aplican restricciones', true, 12, 'corte de pelo', 'corte de pelo unisex', '20000', 'https://www.genesiscareer.edu/wp-content/uploads/2019/06/swedishmassage.jpg', 3);
+VALUES (5, '2', 'No aplican restricciones', true, 12, 'Corte de pelo', 'Corte de pelo unisex', '20000', 'https://i.pinimg.com/originals/5e/5c/bd/5e5cbd4d3517ba13db403b2145f91b07.jpg', 3);
 INSERT INTO SERVICIO_ENTITY (id, duracion, restricciones, disponible, hora_inicio, nombre, descripcion, precio, imagen, sede_id)
-VALUES (6, '1', 'Ser mayor de 16 años', true, 13, 'tinte', 'aplicación de tinte', '35000', 'https://www.genesiscareer.edu/wp-content/uploads/2019/06/swedishmassage.jpg', 3);
+VALUES (6, '1', 'Ser mayor de 16 años', true, 13, 'Tinte', 'Aplicación de tinte', '35000', 'https://i.pinimg.com/originals/5e/5c/bd/5e5cbd4d3517ba13db403b2145f91b07.jpg', 3);
 INSERT INTO SERVICIO_ENTITY (id, duracion, restricciones, disponible, hora_inicio, nombre, descripcion, precio, imagen, sede_id)
-VALUES (7, '3', 'Ser mayor de 18 años', true, 14, 'pedicure', 'tratamiento de pedicure', '30000', 'https://www.genesiscareer.edu/wp-content/uploads/2019/06/swedishmassage.jpg', 4);
+VALUES (7, '3', 'Ser mayor de 18 años', true, 14, 'Pedicure', 'Tratamiento de pedicure', '30000', 'https://i.pinimg.com/originals/5e/5c/bd/5e5cbd4d3517ba13db403b2145f91b07.jpg', 4);
 INSERT INTO SERVICIO_ENTITY (id, duracion, restricciones, disponible, hora_inicio, nombre, descripcion, precio, imagen, sede_id)
-VALUES (8, '2', 'No aplican restricciones', true, 15, 'maquillaje', 'maquillaje profesional', '40000', 'https://www.genesiscareer.edu/wp-content/uploads/2019/06/swedishmassage.jpg', 4);
+VALUES (8, '2', 'No aplican restricciones', true, 15, 'Maquillaje', 'Maquillaje profesional', '40000', 'https://i.pinimg.com/originals/5e/5c/bd/5e5cbd4d3517ba13db403b2145f91b07.jpg', 4);
 INSERT INTO SERVICIO_ENTITY (id, duracion, restricciones, disponible, hora_inicio, nombre, descripcion, precio, imagen, sede_id)
-VALUES (9, '1', 'Ser mayor de 16 años', true, 16, 'peinado', 'peinado de eventos', '25000', 'https://www.genesiscareer.edu/wp-content/uploads/2019/06/swedishmassage.jpg', 5);
+VALUES (9, '1', 'Ser mayor de 16 años', true, 16, 'Peinado', 'Peinado de eventos', '25000', 'https://i.pinimg.com/originals/5e/5c/bd/5e5cbd4d3517ba13db403b2145f91b07.jpg', 5);
 INSERT INTO SERVICIO_ENTITY (id, duracion, restricciones, disponible, hora_inicio, nombre, descripcion, precio, imagen, sede_id)
-VALUES (10, '2', 'Ser mayor de 18 años', true, 17, 'uñas acrílicas', 'aplicación de uñas acrílicas', '45000', 'https://www.genesiscareer.edu/wp-content/uploads/2019/06/swedishmassage.jpg', 5);
+VALUES (10, '2', 'Ser mayor de 18 años', true, 17, 'Uñas acrílicas', 'Aplicación de uñas acrílicas', '45000', 'https://i.pinimg.com/originals/5e/5c/bd/5e5cbd4d3517ba13db403b2145f91b07.jpg', 5);
 INSERT INTO SERVICIO_ENTITY (id, duracion, restricciones, disponible, hora_inicio, nombre, descripcion, precio, imagen, sede_id)
-VALUES (11, '4', 'No aplican restricciones', true, 18, 'masaje deportivo', 'masaje para deportistas', '35000', 'https://www.genesiscareer.edu/wp-content/uploads/2019/06/swedishmassage.jpg', 6);
+VALUES (11, '4', 'No aplican restricciones', true, 18, 'Masaje deportivo', 'Masaje para deportistas', '35000', 'https://i.pinimg.com/originals/5e/5c/bd/5e5cbd4d3517ba13db403b2145f91b07.jpg', 6);
 INSERT INTO SERVICIO_ENTITY (id, duracion, restricciones, disponible, hora_inicio, nombre, descripcion, precio, imagen, sede_id)
-VALUES (12, '2', 'Ser mayor de 16 años', true, 19, 'tratamiento capilar', 'tratamiento para el cabello', '50000', 'https://www.genesiscareer.edu/wp-content/uploads/2019/06/swedishmassage.jpg', 6);
+VALUES (12, '2', 'Ser mayor de 16 años', true, 19, 'Tratamiento capilar', 'Tratamiento para el cabello', '50000', 'https://i.pinimg.com/originals/5e/5c/bd/5e5cbd4d3517ba13db403b2145f91b07.jpg', 6);
 
 -- Create pack 1
 INSERT INTO PACK_DE_SERVICIOS_ENTITY (id, descuento, nombre, imagen, sede_id)
